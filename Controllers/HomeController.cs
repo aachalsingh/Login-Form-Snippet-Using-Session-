0794be59e072cfb6e8d62a61bbe7f3945a944a21
Aachal Singh <aachal.singh07@gmail.com>
2023-11-25 19:26:46 +0530

    Session

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["var1"] = " Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from session";
+
+            string[] Students = { "aa", "bb", "cc" };
+
+            Session["Var5"] = Students;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+
+
+            return View();
+        }
+    }
+}
